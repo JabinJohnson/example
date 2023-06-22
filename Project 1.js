95a391ce9b8c2df0064514958dadd615234b17a8
Jabin Johnson <awesomeJabin1@gmail.com>
2023-06-22 16:34:00 -0400

    project add

---
+++ Project 1.js
@@ -0,0 +1,40 @@
+let pick;
+let number = prompt("How many rounds?");
+switch(number){
+    case "1":
+    let text = '1';
+    let = parseInt(text, 10);
+    break;
+}
+const result = [win,loss,tie];
+let userChoice = prompt("Rock,Paper,or Scissors?");
+while (number <= pick){
+    count++;
+ if (userChoice == getRndInteger) {
+    alert("Tie!")
+    counter++; 
+}
+if (userChoice = 0, getRndInteger = 2) {
+  alert("You Win!")
+  counter++; 
+}
+if (userChoice = 1, getRndInteger = 0){
+  alert("You Win!")
+  counter++
+}
+if (userChoice = 2, getRndInteger = 1){
+  alert("You Win!")
+  counter++
+}
+else{
+  alert("You Lose!")
+  counter++
+}; }
+let Rock = 0;
+let Paper = 1;
+let Scissors = 2;
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  }
+  alert("You have" + result);
+  
