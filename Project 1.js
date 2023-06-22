fed6326f0cfa869c146f801c5f32fed8845dd9b4
Jabin Johnson <awesomeJabin1@gmail.com>
2023-06-22 17:13:35 -0400

    edited

--- Project 1.js
+++ Project 1.js
@@ -1,40 +1,40 @@
-let pick;
-let number = prompt("How many rounds?");
-switch(number){
-    case "1":
-    let text = '1';
-    let = parseInt(text, 10);
-    break;
+let number = parseInt(prompt("How many rounds?"));
+
+let win = 0;
+let lose = 0;
+let tie = 0;
+for(let i=0; i<number; i++){
+  let userChoice = prompt("Rock,Paper,or Scissors?");
+  let random = Math.floor(Math.random() * 3 );
+  let computer = 0;
+  console.log(random);
+  if ()
+  // Translate this random variable to computer choice.
 }
-const result = [win,loss,tie];
-let userChoice = prompt("Rock,Paper,or Scissors?");
-while (number <= pick){
-    count++;
- if (userChoice == getRndInteger) {
-    alert("Tie!")
-    counter++; 
-}
-if (userChoice = 0, getRndInteger = 2) {
-  alert("You Win!")
-  counter++; 
-}
-if (userChoice = 1, getRndInteger = 0){
-  alert("You Win!")
-  counter++
-}
-if (userChoice = 2, getRndInteger = 1){
-  alert("You Win!")
-  counter++
-}
-else{
-  alert("You Lose!")
-  counter++
-}; }
-let Rock = 0;
-let Paper = 1;
-let Scissors = 2;
-function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) ) + min;
-  }
-  alert("You have" + result);
+
+// while (number <= pick){
+//     count++;
+//  if (userChoice == getRndInteger) {
+//     alert("Tie!")
+//     counter++; 
+// }
+// if (userChoice = 0, getRndInteger = 2) {
+//   alert("You Win!")
+//   counter++; 
+// }
+// if (userChoice = 1, getRndInteger = 0){
+//   alert("You Win!")
+//   counter++
+// }
+// if (userChoice = 2, getRndInteger = 1){
+//   alert("You Win!")
+//   counter++
+// }
+// else{
+//   alert("You Lose!")
+//   counter++
+// }; }
+// let Rock = 0;
+// let Paper = 1;
+// let Scissors = 2;
   
